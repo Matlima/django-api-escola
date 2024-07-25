6a3b966f5bdeafc8761d6ba72df27943d1f9ff1c
Matheus <limarios18@outlook.com>
2024-07-24 22:25:18 -0300

    Implementação de Autenticação na API

--- escola/views.py
+++ escola/views.py
@@ -1,8 +1,14 @@
 from escola.models import Estudante, Curso, Matricula
-from escola.serializers import EstudanteSerializer, CursoSerializer, MatriculaSerializer
-from rest_framework import viewsets
+from escola.serializers import EstudanteSerializer, CursoSerializer, MatriculaSerializer, ListaMatriculasCursoSerializer, ListaMatriculasEstudanteSerializer
+from rest_framework import viewsets, generics
+# Caso queira adicionar apenas em um view
+# from rest_framework.authentication import BasicAuthentication
+# from rest_framework.permissions import IsAuthenticated
+
 
 class EstudanteViewSet(viewsets.ModelViewSet):
+    # authentication_classes = [BasicAuthentication]
+    # permission_classes = [IsAuthenticated]
     queryset = Estudante.objects.all()
     serializer_class = EstudanteSerializer
 
@@ -16,7 +22,17 @@ class MatriculaViewSet(viewsets.ModelViewSet):
     queryset = Matricula.objects.all()
     serializer_class = MatriculaSerializer
 
+class ListaMatriculaEstudante(generics.ListAPIView):
+    def get_queryset(self):
+        queryset = Matricula.objects.filter(estudante_id=self.kwargs['pk'])
+        return queryset
+    serializer_class = ListaMatriculasEstudanteSerializer
 
 
+class ListaMatriculaCurso(generics.ListAPIView):
+    def get_queryset(self):
+        queryset = Curso.objects.filter(curso_id=self.kwargs['pk'])
+        return queryset
+    serializer_class = ListaMatriculasCursoSerializer
 
 
